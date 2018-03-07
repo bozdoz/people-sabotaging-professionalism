3b1daf910b1ee102068f2e63e8f026c588ca8d21
bozdoz <howaboutben@gmail.com>
2018-03-07 17:42:45 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,70 @@
+import data from './data.json';
+import './index.scss';
+
+const headline = document.getElementById('headline');
+const container = document.getElementById('headline-container');
+const nextbtn = document.getElementById('next');
+const prevbtn = document.getElementById('prev');
+
+const shuffle = (a) => {
+    var j, x, i;
+    for (i = a.length; i; i--) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+    }
+	return a;
+};
+
+const outdata = shuffle(data.slice());
+const outlen = outdata.length;
+let i = 0;
+
+const update = () => {
+	let newcontent = outdata[i];
+	
+	// add links
+	newcontent = newcontent.replace(/(https?:\/\/\S+)/g, (a) => (
+		`<a href="${a}" target="_blank">${a}</a>`
+	)); 
+
+	// replace line breaks with HTML breaks
+	newcontent = newcontent.replace(/(\/\s)/g, (a) => (
+		`<br />`
+	));	
+
+	headline.innerHTML = newcontent;
+	container.scrollTop = 0;
+};
+
+const next = () => {
+	i = (i + 1) % outlen;
+	update();
+};
+
+const prev = () => {
+	i = (outlen + i - 1) % outlen;
+	update();
+};
+
+update();
+
+nextbtn.addEventListener('click', next);
+prevbtn.addEventListener('click', prev);
+
+window.addEventListener('keydown', (e) => {
+	const { keyCode } = e;
+	if (keyCode === 37) {
+		// left
+		prev();
+	} else if (keyCode === 39) {
+		// right
+		next();
+	}
+});
+
+// hot reloading
+if (module.hot) {
+	module.hot.accept();
+}
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const ENV = process.env.NODE_ENV || 'dev';
+const is_prod = ENV === 'production';
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'js/main.js',
+        path: path.resolve(__dirname, 'docs'),
+        publicPath: '/'
+    },
+    devServer: is_prod ? {} : {
+    	contentBase: './docs',
+        historyApiFallback: true,
+        hot: true
+    },
+    devtool: is_prod ? false : 'cheap-module-eval-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'People Sabotaging Professionalism',
+            description: 'A series of unfortunate events which took place on Earth, a few years ago...',
+            site: 'https://bozdoz.github.io/people-sabotaging-professionalism',
+            template: './src/index.html',
+            links: [
+                'https://fonts.googleapis.com/css?family=Quicksand:500'
+            ]
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.optimize.UglifyJsPlugin()
+    ],
+    module: {
+        rules: [
+        {
+            test: /\.js$/,
+            use: ['babel-loader']
+        },
+        {
+            test: /\.s?css$/,
+            use: [
+                'style-loader',
+                'css-loader?sourceMap',
+                'sass-loader?sourceMap',
+                'postcss-loader?sourceMap'
+            ]
+        },
+        {
+            test: /\.json$/,
+            use: ['json-loader']
+        }]
+    }
+};
